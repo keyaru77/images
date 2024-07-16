621b84d5b5eacb1ec023c0f630871194e1bc05fe
keyaru77 <whyudacok@gmail.com>
2024-07-16 14:27:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
+let views = {};
+
+// Endpoint untuk proxying gambar
 app.get('/img', async (req, res, next) => {
   const imageUrl = req.query.url;
   try {
@@ -19,8 +23,26 @@ app.get('/img', async (req, res, next) => {
   }
 });
 
+// Endpoint untuk mendapatkan jumlah views berdasarkan ID
+app.get('/view/:id', (req, res) => {
+  const id = req.params.id;
+  if (!views[id]) {
+    views[id] = 0;
+  }
+  res.json({ id: id, views: views[id] });
+});
+
+// Endpoint untuk menambah jumlah views berdasarkan ID
+app.post('/view/:id', (req, res) => {
+  const id = req.params.id;
+  if (!views[id]) {
+    views[id] = 0;
+  }
+  views[id] += 1;
+  res.json({ id: id, views: views[id] });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-  
